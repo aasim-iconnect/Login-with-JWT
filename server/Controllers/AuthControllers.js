7d5f57d993ab07b101ced269d87a7f72a90d2f04
aasim-iconnect <aasims@iconnectsolutions.com>
2022-05-26 10:59:12 +0530

    fixed server errors and handleerror

--- server/Controllers/AuthControllers.js
+++ server/Controllers/AuthControllers.js
@@ -7,19 +7,33 @@ const createToken = (id) => {
     expiresIn: maxAge,
   });
 };
+const handleError = (err) => {
+  let errors = { email: "", password: "" };
+  if (err.code === 11000) {
+    errors.email = "Email is already registered";
+  }
+  if (err.message.includes("Users validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
 module.exports.register = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await UserModel.create({ email, password });
     const token = createToken(user._id);
-    response.cookie("jwt", token, {
+    res.cookie("jwt", token, {
       withCredentials: true,
       httpOnly: false,
       maxAge: maxAge * 1000,
     });
-    response.status(201).json({ user: user._id, created: true });
+    res.status(201).json({ user: user._id, created: true });
   } catch (err) {
     console.log(err);
+    const errors = handleError(err);
+    res.json({ errors, created: false });
   }
 };
 module.exports.login = async (req, res, next) => {};
